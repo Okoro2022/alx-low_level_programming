fc1e84332e8d38deb6558d4753d7b4f2f6d27931
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-10 08:55:00 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	float f;
 
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of an int: %lu byte (s)\n", (unsigned long)sizeof(b));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
-	xreturn (0);
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
