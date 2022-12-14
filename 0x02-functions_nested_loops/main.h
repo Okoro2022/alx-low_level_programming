1cab3794b9e98f7ee671d781dadda93c559757d3
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-14 06:58:03 -0800

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File - main.h
- * Author: _alphabet
- * Desdcription: putchar function in. C - Functions, nested loops
- *          used 0x02. C - Functions, nested loops
- * Return: always 0 (success)
- */
 int _putchar(char c);
-int _alphabet(int c);
-
-
-
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+int _natural(char c);
+int _fibonacci(char c);
 
 #endif
