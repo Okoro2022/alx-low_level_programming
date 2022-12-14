fa89239183f2f1a3619aecb7ea614ae35430b58c
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-14 15:18:06 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,32 +11,34 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-		for (mult = 1; mult <= n; mult++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar('0');
 
-			prod = num * mult;
-
-			if (prod <= 99)
-				_putchar(' ');
-
-			if (prod <= 9)
-				_putchar(' ');
-
-			if (prod >= 100)
-
-		        {
-				_putchar((prod / 100) + '0');
-				_putchar(((prod / 10)) % 10 + '0');
-			}
-			else if (prod <= 99 && prod >= 10)
+			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar((prod / 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-			_putchar((prod % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
