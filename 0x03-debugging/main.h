0b2a96e815deffbab7c02592bc227d4200926c64
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-16 05:08:51 -0800

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 
-positive_or_negative(int 1)
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
