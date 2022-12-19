34d2546169133e984d674c406df4d976b4da7c39
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-19 07:28:01 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,16 @@
  * @src: copy from
  * Return: string
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char *d = dest;
 
-	for (; src[i] != '\0'; i++)
+	for (; *src != '\0'; src++)
 	{
-		dest[i] = src[i];
+		*dest = *src;
+		dest++;
 	}
-	return (dest);
+		*dest = *src;
+		return (d);
 }
