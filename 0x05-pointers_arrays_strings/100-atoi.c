8274901bff22d3515d14886bb36b6ac266f4b0f6
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-19 08:35:55 -0800

    Task 10 Adv

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,33 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * @s: string
- * Return: integer
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
  */
-
 int _atoi(char *s)
 {
-	return (*s);
+	short boolean;
+	int i, minus, result;
+
+	i = minus = result = boolean = 0;
+	minus = -1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			minus *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
+			break;
+		i++;
+	}
+	result *= minus;
+	return (result);
 }
