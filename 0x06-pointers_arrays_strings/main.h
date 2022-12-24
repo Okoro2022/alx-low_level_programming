0d612cdc55ba661b587e48e140212aa1a12b441c
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-23 20:32:20 -0800

    Task 8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
-
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
