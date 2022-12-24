5ee3cba08552efc30a2d0941e3f67da06b62c3f3
Tochukwu Okoro <stephanotochigg7@gmail.com>
2022-12-23 23:46:00 -0800

    Task 8 update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,33 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only once in your code.
- * @n: input
- *
- * Return: decrypted string
+ * rot13 - encode using rot13
+ * @s: variable
+ * Return: Always 0 (Success)
  */
-char *rot13(char *n)
+char *rot13(char *s)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int i = 0, i2 = 0;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (n[i] != '\0')
+	while (*(s + i) != 0)
 	{
-		for (x = 0; x <= 51; x++)
+		for (i2 = 0; i2 <= 52; i2++)
 		{
-			if (n[i] == toswap[x])
+			if (*(s + i) == alpha[i2])
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				*(s + i) = alpha2[i2];
+				break;
 			}
-			rot_c = rot_c * -1;
 		}
 		i++;
 	}
-	return (n);
+	return (s);
 }
