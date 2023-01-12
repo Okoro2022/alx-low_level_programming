9ae1ab008d947d10eeb919616eaf9d665ff03f32
Tochukwu Okoro <stephanotochigg7@gmail.com>
2023-01-12 03:06:02 -0800

    task 0

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int check_number(char *str);
+unsigned int string_length(char *str);
+void print_string(char *str);
+void *_calloc(unsigned int number, unsigned int size);
+int _putchar(char c);
+
+#endif
