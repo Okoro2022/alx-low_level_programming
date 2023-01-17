2a2fb03e173b101ad99018f1a37343cc45e13a12
Tochukwu Okoro <stephanotochigg7@gmail.com>
2023-01-16 19:32:33 -0800

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ * free_dog - frees memory of structure dogs
+ * @d: pointer of structure
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d);
+	if (d != 0)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
