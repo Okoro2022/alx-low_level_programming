8d9b4f24805520e9009ac2681f31c6775336ef81
Steve2022 <stephanotochigg7@gmail.com>
2023-03-17 06:40:16 -0700

    Task 5 adv

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 /**
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
@@ -16,6 +23,7 @@ int find_len(char *str)
 
 	return (len);
 }
+
 /**
  * create_xarray - Creates an array of chars and initializes it with
  *                 the character 'x'. Adds a terminating null byte.
@@ -42,6 +50,7 @@ char *create_xarray(int size)
 
 	return (array);
 }
+
 /**
  * iterate_zeroes - Iterates through a string of numbers containing
  *                  leading zeroes until it hits a non-zero number.
@@ -56,6 +65,7 @@ char *iterate_zeroes(char *str)
 
 	return (str);
 }
+
 /**
  * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
@@ -76,6 +86,7 @@ int get_digit(char c)
 
 	return (digit);
 }
+
 /**
  * get_prod - Multiplies a string of numbers by a single digit.
  * @prod: The buffer to store the result.
@@ -124,6 +135,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	if (tens)
 		*prod = (tens % 10) + '0';
 }
+
 /**
  * add_nums - Adds the numbers stored in two strings.
  * @final_prod: The buffer storing the running final product.
@@ -165,6 +177,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 	if (tens)
 		*final_prod = (tens % 10) + '0';
 }
+
 /**
  * main - Multiplies two positive numbers.
  * @argv: The number of arguments passed to the program.
