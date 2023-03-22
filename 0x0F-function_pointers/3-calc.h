32184d5cc59b5ec7e4f8c51989f861434ff61e97
Steve2022 <stephanotochigg7@gmail.com>
2023-03-22 15:13:02 -0700

    Task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
-#ifndef STRUCT_H
-#define STRUCT_H
-#include <stddef.h>
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct op - Struct op
  *
@@ -12,14 +11,11 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-#endif
 
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 #endif
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,75 @@
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "3-calc.h"
+
 /**
- * op_add - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_add - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_sub - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_mul - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_div - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
+
 /**
- * op_mod - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
