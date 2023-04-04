89c41b82f8845e784131c211e36ec6d40ad6ea59
Steve2022 <stephanotochigg7@gmail.com>
2023-04-04 16:23:49 -0700

    Task 11

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,58 +1,35 @@
-#include "main.h"
-
+#include "lists.h"
 /**
- * delete_nodeint_at_index - deletes a node at an index
- * @head: pointer to the head of the list
- * @index: index of the node to be added
- *
- * Return: the address of the node
+ * delete_nodeint_at_index -deletes a node
+ * @head: pointer to the first node
+ * @idx: index to the first node
+ * Return: 1 if success, - 1 if fails
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, unsigned int idx)
 {
-	listint_t *old_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0, list_len = listint_len(*head);
+	listint_t *temp, *temp_addr;
+	unsigned int count;
 
-	if ((index > list_len) || (list_len == 0))
-		return (-1);
-	while (head != NULL)
+	temp = *head;
+	count = 0;
+	while (temp)
 	{
-		if (i == index)
+		if (count == (idx - 1))
 		{
-			old_node = *head;
-			if (i == 0)
-			{
-				*head = old_node->next;
-				free(old_node);
-				return (1);
-			}
-			previous_node->next = old_node->next;
-			free(old_node);
+			temp_addr = (temp->next)->next;
+			free(temp->next);
+			temp->next = temp_addr;
 			return (1);
 		}
-		else if ((i + 1) == index)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
+		else if (idx == 0)
+		{
+			temp = *head;
+			*head = (*head)->next;
+			free(temp);
+			return (1);
+		}
+		count++;
+		temp = temp->next;
 	}
 	return (-1);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
 }
