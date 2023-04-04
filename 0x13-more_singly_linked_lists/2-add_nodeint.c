ba10f5b15e7d1d506fb500c7bcaa84764dbeb7b0
Steve2022 <stephanotochigg7@gmail.com>
2023-04-04 16:29:49 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,58 +1,25 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node at an index
- * @head: pointer to the head of the list
- * @index: index of the node to be added
+ * add_nodeint - add a new node at the beginning
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
  *
- * Return: the address of the node
+ * Return: address of the new element. NUll if it failed.
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *old_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0, list_len = listint_len(*head);
+	listint_t *new;
 
-	if ((index > list_len) || (list_len == 0))
-		return (-1);
-	while (head != NULL)
-	{
-		if (i == index)
-		{
-			old_node = *head;
-			if (i == 0)
-			{
-				*head = old_node->next;
-				free(old_node);
-				return (1);
-			}
-			previous_node->next = old_node->next;
-			free(old_node);
-			return (1);
-		}
-		else if ((i + 1) == index)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
-	}
-	return (-1);
-}
+	new = malloc(sizeof(listint_t));
 
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
+	if (new == NULL)
+		return (NULL);
 
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
 }
