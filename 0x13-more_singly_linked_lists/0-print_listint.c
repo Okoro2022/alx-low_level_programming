57ab8ecf0c8df27ca061bfe9e31443e15e6299a9
Steve2022 <stephanotochigg7@gmail.com>
2023-04-04 18:14:01 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,27 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a struct list type listint_t
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
- * @h: pointer to the head/first member of a singly linked list of structs
- * of type listint_s
- *
- * Return: amount of nodes in the list
+ * Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 1;
+const listint_t *cursor = h;
+size_t count = 0;
 
-	if (h)
-	{
-		printf("%i\n", h->n);
-
-		if (h->next)
-			count += print_listint(h->next);
-
-		return (count);
-	}
-	else
-		return (0);
+while (cursor != NULL)
+{
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
+}
+return (count);
 }
