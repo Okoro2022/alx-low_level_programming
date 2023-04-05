2d922b079e4f413422d3247c8f091b181072fb17
Steve2022 <stephanotochigg7@gmail.com>
2023-04-04 17:20:47 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,63 +1,29 @@
 #include "lists.h"
-
 /**
- * _ra - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
- */
-listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+* free_listint_safe - frees the list
+* @h: head
+* Return: number
+*/
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t **newlist;
-	size_t i;
+size_t a;
+listint_t *b;
 
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
+a = 0;
+if (!h)
+return (a);
+while (*h && *h > (*h)->next)
+{
+b = *h;
+*h = (*h)->next;
+free(b);
+a++;
 }
-
-/**
- * free_listint_safe - frees a listint_t linked list.
- * @head: double pointer to the start of the list
- *
- * Return: the number of nodes in the list
- */
-size_t free_listint_safe(listint_t **head)
+if (*h)
 {
-	size_t i, num = 0;
-	listint_t **list = NULL;
-	listint_t *next;
-
-	if (head == NULL || *head == NULL)
-		return (num);
-	while (*head != NULL)
-	{
-		for (i = 0; i < num; i++)
-		{
-			if (*head == list[i])
-			{
-				*head = NULL;
-				free(list);
-				return (num);
-			}
-		}
-		num++;
-		list = _ra(list, num, *head);
-		next = (*head)->next;
-		free(*head);
-		*head = next;
-	}
-	free(list);
-	return (num);
+free(*h);
+*h = NULL;
+a++;
+}
+return (a);
 }
